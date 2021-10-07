68c12c62f30ce4ee1e84be5a3af6e7bcb7fd2eee
tonip57 <74241142+tonip57@users.noreply.github.com>
2021-10-07 14:07:44 +0300

    Add files via upload

---
+++ osa3/puhelinluettelo/README.md
@@ -0,0 +1,3 @@
+https://puhelinluettelo73737.herokuapp.com/api/persons
+
+Kaikki tehtävät 3.12 asti
---
+++ osa3/puhelinluettelo/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.qnwi1.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv[3] != undefined || process.argv[3] === "") {
+    const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+    })
+
+    person.save().then(response => {
+        console.log('added ' + process.argv[3] + ' number ' + process.argv[4] + ' to phonebook')
+        mongoose.connection.close()
+    })
+} else {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+        console.log(person.name + " " + person.number)
+        })
+    mongoose.connection.close()
+    })
+}
