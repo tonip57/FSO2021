3dfb2c542291ce4dc49a47236df46385a3fe4ad7
tonip57 <74241142+tonip57@users.noreply.github.com>
2021-10-22 17:04:03 +0300

    Add files via upload

---
+++ controllers/blogs.js
@@ -0,0 +1,79 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const middleware = require('../utils/middleware')
+
+blogsRouter.get('/', async (request, response) => { 
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
+  const body = request.body
+  
+  const user = request.user
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+  
+  
+  if ('title' in blog && 'url' in blog && blog.title !== undefined && blog.url !== undefined) {
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(200).send(savedBlog)
+  } else {
+    return response.status(400).send('Bad Request')
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  if (updatedBlog) {
+    response.json(updatedBlog)
+  } else {
+    response.status(400).end()
+  }
+})
+
+
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  if(request.token === undefined) {
+    response.status(401).send('Unauthorized')
+  }
+  
+  const user = request.user
+  
+  const blog = await Blog.findById(request.params.id)
+  
+  if ( blog.user.toString() === user.id.toString() ) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    response.status(401).send('Unauthorized')
+  }
+})
+
+
+module.exports = blogsRouter
---
+++ tests/favoriteBlog.test.js
@@ -0,0 +1,123 @@
+const listHelper = require('../utils/list_helper')
+
+describe('favorite blog', () => {
+      const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      },
+      {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+      },
+      {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+      }  
+    ]
+
+    const listWithOneBlog = [
+    {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+    },
+    ]
+    
+    const listWithSameAmountOfLikes = [
+    {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 12,
+    __v: 0
+    }  
+    ]
+    
+    const emptyList = []
+    
+    const answer = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12
+    }
+    
+    const answer2 = undefined
+    
+
+   test('favorite blog of big list', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(answer)
+  })
+  
+  test('favorite blog of list of one', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(answer)
+  })
+  
+  test('favorite blog of empty list', () => {
+    const result = listHelper.favoriteBlog(emptyList)
+    expect(result).toBe(answer2)
+  })
+
+  test('favorite blog of a list with multiple correct answers', () => {
+    const result = listHelper.favoriteBlog(listWithSameAmountOfLikes)
+    expect(result).toEqual(answer)
+  })
+})
